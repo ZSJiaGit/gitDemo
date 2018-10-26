34058cf0a54d9a2adfc57c2d30331fcc8dd68f0e
ZSongJia <2539782164@qq.com>
2018-10-26 19:19:35 +0800

    分支提交

--- src/main/java/cn/itcast/test/GitTest.java
+++ src/main/java/cn/itcast/test/GitTest.java
@@ -3,5 +3,7 @@ package cn.itcast.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("88899777");
+        System.out.println("这是分支");
+        System.out.println("这是分支");
     }
 }
